1e18c8cb31e655d3c599a02e033e429ee6d22dca
Ananya Singh <99748964+2456373736@users.noreply.github.com>
2024-12-03 18:13:54 +0530

    Add files via upload

---
+++ Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Welcome() {
+
+    return(
+        <>
+        <h1>Hello!</h1>
+        <p><i>Greetings of the day!</i></p>
+        </>
+    )
+};
+export default Welcome;

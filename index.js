bf41c708b8a933746d801c61a228c756f2a63976
Ananya Singh <99748964+2456373736@users.noreply.github.com>
2024-12-16 21:02:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+const redux= require('redux');
+const reduxLogger= require('redux-logger')
+const middleWare= redux.applyMiddleware
+const logger= reduxLogger.createLogger();
+const createStore= redux.createStore;
+const combineReducer= redux.combineReducers;
+const BUY_CAKE= "BUY_CAKE";
+const BUY_ICECREAM= "BUY_ICECREAM";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-    
-  </React.StrictMode>
-);
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'Our First Redux Action.'
+    }
+}
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM,
+    }
+}
+const initialCakeState= {
+    numberOfCakes: 10
+}
+const initialIceCreamState= {
+    numberOfIceCreams: 20
+}
+//(previousState, action) => newState (reducer does not mutate the old state it returns a new state)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const cakeReducer =(state= initialCakeState,action)=>{
+
+    switch(action.type)
+    {
+        case 'BUY_CAKE': return {
+          ...state,
+             numberOfCakes: state.numberOfCakes-1
+            }
+        default: return state 
+
+    }
+
+}//redux app:
+
+const iceCreamReducer =(state= initialIceCreamState,action)=>{
+
+    switch(action.type)
+    {
+        case 'BUY_ICECREAM': return {
+                ...state,
+                numberOfIceCreams: state.numberOfIceCreams-1
+            }
+        default: return state 
+
+    }
+
+}//redux app:
+const rootReducer= combineReducer({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+const store= createStore(rootReducer,middleWare(logger));
+console.log('Initial State: ', store.getState())
+const unsubscribe= store.subscribe(()=>{})//console.log('Updated State: ',store.getState())})
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+unsubscribe();
+
